b4d9076094e70228e7fa8a84499e3ef4387c6734
mariyaabk <abaakilmariya@gmail.com>
2025-04-25 12:22:44 +0200

    branch mariya release 1

--- lootopia/components/Navbar.js
+++ lootopia/components/Navbar.js
@@ -1,15 +1,24 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 export default function Navbar() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    // Vérifie si le code s'exécute côté client
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem('token');
+      setIsAuthenticated(token !== null);
+    }
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
+    setIsAuthenticated(false); // Mettre à jour l'état après déconnexion
     router.push('/auth/login');
   };
-
-  const isAuthenticated = localStorage.getItem('token') !== null;
 
   return (
     <nav className="bg-gray-900 p-4 flex justify-between items-center text-white">
